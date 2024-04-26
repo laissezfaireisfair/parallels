29962edb7b16ecc5978e6214df5a67d40fd1d265
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-04-26 19:56:16 +0700

    Make server

--- Task3_ClientServer/include/Server.h
+++ Task3_ClientServer/include/Server.h
@@ -4,62 +4,92 @@
 #include <future>
 #include <thread>
 #include <unordered_map>
+#include <optional>
+#include <functional>
 
 namespace parallels {
-using std::size_t;
-using std::queue;
-using std::packaged_task;
-using std::jthread;
 using std::bind;
-using std::pair;
+using std::placeholders::_1;
+using std::nullopt;
+using std::packaged_task;
+using std::scoped_lock;
+using std::logic_error;
+using std::future;
+using std::optional;
+using std::jthread;
+using std::queue;
 using std::unordered_map;
+using std::size_t;
+using std::mutex;
+using std::stop_token;
 using std::this_thread::sleep_for;
 using std::chrono::milliseconds;
-using std::stop_token;
-using std::mutex;
 
 template<class T>
 class Server {
  public:
   void Start() {
-    is_active_ = true;
+    if (computingThread)
+      return;
 
-    while (is_active_) {
-      if (!tasks_and_ids_.empty()) {
-        auto pair = std::move(tasks_and_ids_.front());
-        pair.second()();
-        results_by_task_id_.emplace(pair.first(), pair.second().get_future().get());
-        tasks_and_ids_.pop();
-      }
-      sleep_for(milliseconds(50));
-    }
+    computingThread.emplace(bind(&Server::computeTasksJob, this, _1));
   }
 
   void Stop() {
-    is_active_ = false;
+    if (!computingThread)
+      return;
+
+    computingThread->request_stop();
+    computingThread = nullopt;
   }
 
   size_t AddTask(packaged_task<T()> task) {
-    tasks_and_ids_.emplace(std::move(task), ++id_counter_);
-    return id_counter_;
+    scoped_lock lock(tasks_mutex_);
+
+    auto new_id = id_counter_++;
+    {
+      scoped_lock futures_lock(futures_mutex_);
+      future_by_id[new_id] = task.get_future();
+    }
+    tasks_.push(std::move(task));
+
+    return new_id;
   }
 
   T RequestResult(size_t task_id) {
-    if (results_by_task_id_.contains(task_id))
-      return results_by_task_id_[task_id];
+    if (!future_by_id.contains(task_id))
+      throw logic_error("No such result. May be already ejected");
 
-    sleep_for(milliseconds(50));
+    future<T> future;
+    {
+      scoped_lock futures_lock(futures_mutex_);
+      future = std::move(future_by_id[task_id]);
+      future_by_id.erase(task_id);
+    }
+
+    return future.get();
   }
 
  private:
-  queue<pair<size_t, packaged_task<T()>>> tasks_and_ids_;
-
-  unordered_map<size_t, T> results_by_task_id_;
-
+  optional<jthread> computingThread;
+  queue<packaged_task<int()>> tasks_;
+  unordered_map<size_t, future<T>> future_by_id;
   size_t id_counter_ = 0;
+  mutex tasks_mutex_;
+  mutex futures_mutex_;
 
-  mutex mu;
+  void computeTasksJob(stop_token const& token) {
+    while (!token.stop_requested()) {
+      if (!tasks_.empty()) {
+        scoped_lock lock(tasks_mutex_);
+        auto task = std::move(tasks_.front());
+        task();
+        tasks_.pop();
+        continue;
+      }
 
-  void processTasks
+      sleep_for(milliseconds(200));
+    }
+  }
 };
 }
--- Task3_ClientServer/src/main.cpp
+++ Task3_ClientServer/src/main.cpp
@@ -1,55 +1,22 @@
-#include <functional>
 #include <iostream>
-#include <thread>
-#include <future>
-#include <queue>
+#include "Server.h"
 
-int f(int arg) {
-  return arg * arg;
-}
-
-std::queue<std::packaged_task<int()>> tasks_;
-std::mutex mu;
-
-void computeTasksJob(std::stop_token const& token) {
-  while (!token.stop_requested()) {
-    if (!tasks_.empty()) {
-      std::scoped_lock lock(mu);
-      auto task = std::move(tasks_.front());
-      task();
-      tasks_.pop();
-      continue;
-    }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-  }
-}
-
-void clientJob() {
-  auto futures = std::vector<std::future<int>>();
-
-  for (int i = 0; i < 5; ++i) {
-    auto task = std::packaged_task([i] { return f(i); });
-    futures.push_back(task.get_future());
-    {
-      std::scoped_lock lock(mu);
-      tasks_.push(std::move(task));
-    }
-  }
-
-  for (auto& future : futures) {
-    std::cout << future.get() << std::endl;
-  }
-}
+using parallels::Server;
+using std::packaged_task;
+using std::cout;
+using std::endl;
 
 int main() {
-  auto compute_tasks_thread = std::jthread(computeTasksJob);
+  auto server = Server<int>();
+  server.Start();
 
-  auto client_thread = std::jthread(clientJob);
+  auto task = packaged_task<int()>([]() { return 1000 * 1000; });
+  auto task_id = server.AddTask(std::move(task));
 
-  client_thread.join();
+  auto result = server.RequestResult(task_id);
+  cout << result << endl;
 
-  compute_tasks_thread.request_stop();
+  server.Stop();
 
   return 0;
 }
