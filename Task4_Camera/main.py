5ba2c7d593dd507bba9a6620b363f0e1881a21f5
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-07 21:16:00 +0700

    Make sensorX's work

--- Task4_Camera/main.py
+++ Task4_Camera/main.py
@@ -1,8 +1,7 @@
-import threading
 from logging import getLogger, basicConfig, INFO
 from pathlib import Path
 from queue import Queue
-from threading import Thread
+from threading import Thread, Event
 from time import sleep
 
 from ProgramArguments import ProgramArguments
@@ -11,14 +10,22 @@ from SensorX import SensorX
 logger = getLogger(__name__)
 
 
-def sensor_x_job(stop_event, delay: int, result_queue: Queue):
+def sensor_x_job(stop_event, delay: float, update_period: float, result_queue: Queue):
     sensor = SensorX(delay)
-    while not stop_event.wait(1):
+    while not stop_event.wait(update_period):
         result_queue.put(sensor.get())
-        sleep(delay)
+
+
+def get_last(queue: Queue, default: float) -> float:
+    last = default
+    while not queue.empty():
+        last = queue.get()
+    return last
 
 
 def main():
+    sensor_request_period = 0.0001
+
     Path('logs/').mkdir(exist_ok=True)
     basicConfig(level=INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                 datefmt='%d-%m-%y %H:%M:%S', filename='logs/info_plus.log', filemode='a')
@@ -32,25 +39,37 @@ def main():
         return
     logger.info(f'Program arguments: {program_arguments}')
 
-    logger.info('Creating thread...')
+    logger.info('Creating threads...')
     try:
-        stop_event = threading.Event()
-        delay = 1
-        result_queue = Queue()
-        sensor_x_thread = Thread(target=sensor_x_job, args=(stop_event, delay, result_queue))
+        stop_event = Event()
+        low_delay_results = Queue()
+        low_delay_thread = Thread(target=sensor_x_job,
+                                  args=(stop_event, 0.0001, sensor_request_period, low_delay_results))
+        low_delay_thread.start()
+
+        mid_delay_results = Queue()
+        mid_delay_thread = Thread(target=sensor_x_job,
+                                  args=(stop_event, 0.01, sensor_request_period, mid_delay_results))
+        mid_delay_thread.start()
+
+        high_delay_results = Queue()
+        high_delay_thread = Thread(target=sensor_x_job,
+                                   args=(stop_event, 1, sensor_request_period, high_delay_results))
+        high_delay_thread.start()
     except Exception as exception:
         logger.error(exception)
         return
 
-    logger.info('Starting thread...')
-    try:
-        sensor_x_thread.start()
-    except Exception as exception:
-        logger.error(exception)
-        return
-
-    logger.info('Waiting 10 sec...')
-    sleep(10)
+    duration = 3
+    last_low_delay_result = None
+    last_mid_delay_result = None
+    last_high_delay_result = None
+    for _ in range(round(duration * program_arguments.result_frequency)):
+        last_low_delay_result = get_last(low_delay_results, last_low_delay_result)
+        last_mid_delay_result = get_last(mid_delay_results, last_mid_delay_result)
+        last_high_delay_result = get_last(high_delay_results, last_high_delay_result)
+        print(f'{last_low_delay_result} {last_mid_delay_result} {last_high_delay_result}')
+        sleep(1 / program_arguments.result_frequency)
 
     logger.info('Invoking stop event...')
     try:
@@ -60,7 +79,9 @@ def main():
         return
 
     logger.info('Waiting for thread to join...')
-    sensor_x_thread.join()
+    low_delay_thread.join()
+    mid_delay_thread.join()
+    high_delay_thread.join()
 
     logger.info('Finish')
 
