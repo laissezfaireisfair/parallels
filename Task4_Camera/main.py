38abb43851c3c40a4f6d504608c90c73cae70ad5
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-07 17:01:39 +0700

    Add rest of class skeletones

--- Task4_Camera/main.py
+++ Task4_Camera/main.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-import logging
+from logging import getLogger, basicConfig, INFO
 from time import sleep
-import threading
-import queue
-logger = logging.getLogger(__name__)
+from threading import Thread
+from argparse import ArgumentParser
+
+logger = getLogger(__name__)
 
 
 class Sensor:
@@ -25,44 +26,83 @@ class SensorX(Sensor):
 
 
 class SensorCam(Sensor):
-    def __init__(self):
+    def __init__(self, name: str, resolution: str):
         super().__init__(is_valid_child=True)
-        raise NotImplementedError('Not yet implemented')
+        self.name = name
+        self.resolution = resolution
 
     def get(self):
         raise NotImplementedError('Not yet implemented')
 
     def __del__(self):
+        pass
+
+
+class WindowImage:
+    def __init__(self, frequency: float):
+        self.frequency = frequency
+
+    def show(self, image):
         raise NotImplementedError('Not yet implemented')
 
+    def __del__(self):
+        pass
 
-def job():
-    logger.info("Job started")
+
+class ProgramArguments:
+    def __init__(self):
+        parser = ArgumentParser(description='Sensor')
+        parser.add_argument('cameraName', type=str, help='System name of the camera')
+        parser.add_argument('cameraResolution', type=str, help='Camera resolution')
+        parser.add_argument('resultFrequency', type=str, help='Result update frequency')
+
+        args = parser.parse_args()
+        self.camera_name = args.cameraName
+        self.camera_resolution = args.cameraResolution
+        self.camera_frequency = float(args.cameraFrequency)
+
+    def __str__(self):
+        return f'{self.camera_name} {self.camera_resolution} {self.camera_frequency}'
+
+
+def job(program_arguments: ProgramArguments):
+    logger.info(f'Job started with program arguments: {program_arguments}')
 
     sleep(0.5)
 
-    cam_sensor = SensorCam()
+    cam_sensor = SensorCam(program_arguments.camera_name, program_arguments.camera_resolution)
 
     fast_sensor = SensorX(0.01)
     mid_sensor = SensorX(0.1)
     slow_sensor = SensorX(1)
 
-    logger.info("Job finished")
+    logger.info('Job finished')
 
 
 def main():
-    Path("logs/").mkdir(exist_ok=True)
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
-                        datefmt='%d-%m-%y %H:%M:%S', filename='logs/info_plus.log', filemode='a')
-    logger.info("Start up")
+    Path('logs/').mkdir(exist_ok=True)
+    basicConfig(level=INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                datefmt='%d-%m-%y %H:%M:%S', filename='logs/info_plus.log', filemode='a')
+    logger.info('Start up')
 
-    thread = threading.Thread(target=job)
+    logger.info('Parsing arguments...')
+
+    try:
+        program_arguments = ProgramArguments()
+    except Exception as exception:
+        logger.error(exception)
+        return
+
+    logger.info('Starting thread...')
+
+    thread = Thread(target=job, args=(program_arguments,))
     thread.start()
 
-    logger.info("Now waiting for thread to finish")
+    logger.info('Waiting for thread to finish...')
 
     thread.join()
-    logging.info("Finish")
+
+    logger.info('Finish')
 
 
 main()
