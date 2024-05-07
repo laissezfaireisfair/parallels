f4549c5df65e0dfd5827d1fe552c623a47d73205
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-07 16:01:56 +0700

    Add sensors skeleton

--- Task4_Camera/main.py
+++ Task4_Camera/main.py
@@ -2,12 +2,51 @@ from pathlib import Path
 import logging
 from time import sleep
 import threading
+import queue
 logger = logging.getLogger(__name__)
+
+
+class Sensor:
+    def __init__(self, is_valid_child: bool = False):
+        if not is_valid_child:
+            raise RuntimeError('Cannot instantiate abstract class Sensor')
+
+
+class SensorX(Sensor):
+    def __init__(self, delay: float):
+        super().__init__(is_valid_child=True)
+        self.delay = delay
+        self.data = 0
+
+    def get(self) -> int:
+        sleep(self.delay)
+        self.data += 1
+        return self.data
+
+
+class SensorCam(Sensor):
+    def __init__(self):
+        super().__init__(is_valid_child=True)
+        raise NotImplementedError('Not yet implemented')
+
+    def get(self):
+        raise NotImplementedError('Not yet implemented')
+
+    def __del__(self):
+        raise NotImplementedError('Not yet implemented')
 
 
 def job():
     logger.info("Job started")
+
     sleep(0.5)
+
+    cam_sensor = SensorCam()
+
+    fast_sensor = SensorX(0.01)
+    mid_sensor = SensorX(0.1)
+    slow_sensor = SensorX(1)
+
     logger.info("Job finished")
 
 
