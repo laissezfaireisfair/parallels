e7100d955a5c9b4c049643217e800f1faa9da681
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-15 03:57:10 +0700

    Add multithreading

--- Task5_Yolo/main.py
+++ Task5_Yolo/main.py
@@ -2,7 +2,22 @@ from argparse import ArgumentParser
 
 import cv2
 from ultralytics import YOLO
-from time import time
+from time import time, sleep
+from queue import Queue
+from threading import Thread, Event
+
+
+def worker(stop_event: Event, queue: Queue, results: dict):
+    model = YOLO('yolov8s-pose.pt')
+
+    while not stop_event.is_set():
+        if queue.empty():
+            sleep(0.01)
+            continue
+
+        (frame, index) = queue.get()
+        annotated_frame = model.predict(frame, device='cpu')[0].plot()
+        results[index] = annotated_frame
 
 
 def main():
@@ -14,26 +29,52 @@ def main():
     args = parser.parse_args()
     video_path = args.vidPath
     mode = args.mode
-    if mode != 'singlethread':
-        print('Unknown mode. Expected singlethread Exiting...')
+    if mode != 'singlethread' and mode != 'multithread':
+        print('Unknown mode. Expected singlethread or multithread Exiting...')
         return
     output_name = args.outputName
 
-    model = YOLO('yolov8s-pose.pt')
-
     video = cv2.VideoCapture(video_path)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     writer = cv2.VideoWriter(output_name, fourcc, 20.0, (640, 480))
 
     processing_begin_time = time()
 
+    # model = YOLO('yolov8s-pose.pt')
+    #
+    # while video.isOpened():
+    #     ret, frame = video.read()
+    #     if not ret:
+    #         print('Cannot read frame. Exiting...')
+    #         break
+    #     annotated_frame = model.predict(frame, device='cpu')[0].plot()
+    #     writer.write(annotated_frame)
+
+    task_queue = Queue()
+    result_by_index = {}
+
+    stop_event = Event()
+    threads_count = 8
+    threads = [Thread(target=worker, args=(stop_event, task_queue, result_by_index)) for _ in range(threads_count)]
+    for thread in threads:
+        thread.start()
+
+    counter = 0
     while video.isOpened():
         ret, frame = video.read()
         if not ret:
-            print('Cannot read frame. Exiting...')
             break
-        annotated_frame = model(frame)[0].plot()
-        writer.write(annotated_frame)
+        task_queue.put((frame, counter))
+        counter += 1
+
+    while not task_queue.empty():
+        sleep(0.01)
+    stop_event.set()
+
+    counter = 0
+    while result_by_index[counter] is not None:
+        writer.write(result_by_index[counter])
+        counter += 1
 
     process_time = time() - processing_begin_time
 
