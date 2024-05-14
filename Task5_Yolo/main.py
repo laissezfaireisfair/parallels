2611712b89258054869580acbd9167569c820e37
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-15 02:50:01 +0700

    Write to file and measure singlethreaded

--- Task5_Yolo/main.py
+++ Task5_Yolo/main.py
@@ -2,6 +2,8 @@ from argparse import ArgumentParser
 
 import cv2
 from ultralytics import YOLO
+from time import time
+
 
 def main():
     parser = ArgumentParser(description='Yolo')
@@ -12,22 +14,34 @@ def main():
     args = parser.parse_args()
     video_path = args.vidPath
     mode = args.mode
+    if mode != 'singlethread':
+        print('Unknown mode. Expected singlethread Exiting...')
+        return
     output_name = args.outputName
 
     model = YOLO('yolov8s-pose.pt')
 
     video = cv2.VideoCapture(video_path)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    writer = cv2.VideoWriter(output_name, fourcc, 20.0, (640, 480))
+
+    processing_begin_time = time()
 
     while video.isOpened():
-        _, frame = video.read()
-        cv2.imshow('frame', frame)
-        annotated_frame = model(frame)[0].plot()
-        cv2.imshow('annotated frame', annotated_frame)
-        if cv2.waitKey(20) == ord('q'):
+        ret, frame = video.read()
+        if not ret:
+            print('Cannot read frame. Exiting...')
             break
+        annotated_frame = model(frame)[0].plot()
+        writer.write(annotated_frame)
 
+    process_time = time() - processing_begin_time
+
+    writer.release()
     video.release()
     cv2.destroyAllWindows()
+
+    print(f'Processing took {process_time:.2f} seconds')
 
 
 main()
