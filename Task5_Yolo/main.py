0685185097ba2a231189f285060fc88ae37d361e
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-15 01:33:55 +0700

    Add project fot Task 4: Yolo

---
+++ Task5_Yolo/main.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+
+
+def main():
+    parser = ArgumentParser(description='Yolo')
+    parser.add_argument('vidPath', type=str, help='Path to video')
+    parser.add_argument('mode', type=str, help='Single-threaded or multi-threaded')
+    parser.add_argument('outputName', type=str, help='Name of output file')
+
+    args = parser.parse_args()
+    video_path = args.vidPath
+    mode = args.mode
+    output_name = args.outputName
+
+    # TODO: Implement
+    pass
+
+
+main()
