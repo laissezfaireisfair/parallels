cbb38e85875a2e1d2552dbb2534602c9702b38cc
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-05-14 21:33:41 +0700

    Fix leave on q

--- Task4_Camera/WindowImage.py
+++ Task4_Camera/WindowImage.py
@@ -7,13 +7,13 @@ logger = getLogger(__name__)
 
 class WindowImage:
     def __init__(self, frequency: float):
-        self.frequency = frequency  # TODO: Consider getting rid of
+        self.delay = round(1000 / frequency)
         self.window_name = 'Image window'
         self.is_close_requested = False
 
     def show(self, image):
         cv2.imshow(self.window_name, image)
-        if cv2.waitKey(1) == ord('q'):
+        if cv2.waitKey(self.delay) == ord('q'):
             self.is_close_requested = True
 
     def __del__(self):
